fef44ff034776cd1b025a76b0fabf7fdc7c63d57
Marco A <marco.a.braghim@gmail.com>
2019-04-14 11:31:11 -0300

    Database connection ready

--- localhost/site/modules/ModuleAdmin/Controllers/ArtistasController.php
+++ localhost/site/modules/ModuleAdmin/Controllers/ArtistasController.php
@@ -9,6 +9,15 @@ class ArtistasController extends AbstractController
     
     $this->addMsg('Lista de artistas');
 
+    // $mysql = new \SuitUp\Database\DbAdapter\Mysql(array(
+    //   'host' => '192.168.15.2',
+    //   'dbname' => 'suitup',
+    //   'username' => 'suitup',
+    //   'password' => '123456'
+    // ));
+    
+    // $db = new \SuitUp\Database\DbAdapter($mysql);
+
   }
 }
 
